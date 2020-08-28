c41046dd460e1da0a2ad40d3bb8de7189d89d339
Ratnadeep Paul <65324740+Ratnadeep-Paul@users.noreply.github.com>
2020-08-28 16:21:37 +0530

    Source Code Of Modules In Python Tutorial
    This is the Source Code Of Modules In Python Tutorial, Which I uploaded on TechVives.com

--- Py2 Modules In Python.py
+++ Py2 Modules In Python.py
@@ -2,9 +2,11 @@ import time
 
 print("Hii, I am")
 time.sleep(3)
+# This stop the Run of next codes for 3 seconds
 print("TechVives")
 
-# Installation Modules Using pip
+
+# Process Of Installation Modules Using pip
 """
 pip install <Module Name>
 """
