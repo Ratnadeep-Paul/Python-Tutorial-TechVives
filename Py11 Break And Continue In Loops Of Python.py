17c56f3ff59ece0e266b3195851f5c95eb082cd7
Ratnadeep Paul <65324740+Ratnadeep-Paul@users.noreply.github.com>
2020-09-03 22:01:30 +0530

    Source Code Of Break And Continue In Loop Tutorial
    This Is The Source Code Of Break And Continue In Loop Tutorial, Which I Uploaded On TechVives.com

--- Py11 Break And Continue In Loops Of Python.py
+++ Py11 Break And Continue In Loops Of Python.py
@@ -1,28 +1,28 @@
 ## Use Of Break Statement
-# i = 0
-# while (True):
-# 	i = i+1
-# 	print(i)
-# 	if i==24:
-# 		break
+i = 0
+while (True):
+	i = i+1
+	print(i)
+	if i==24:
+		break
 
-## Example of Break statement with "end="
-# i = 0
-# while (True):
-# 	i = i+1
-# 	print(i, end=" ")
-# 	if i==24:
-# 		break
+# Example of Break statement with "end="
+i = 0
+while (True):
+	i = i+1
+	print(i, end=" ")
+	if i==24:
+		break
 
-## Use Of Continue in loops
-# a = 0
-# while True:
-# 	a = a+1
-# 	if a<5:
-# 		continue
-# 	print(a, end=" | ")
-# 	if a==20:
-# 		break
+# Use Of Continue in loops
+a = 0
+while True:
+	a = a+1
+	if a<5:
+		continue
+	print(a, end=" | ")
+	if a==20:
+		break
 
 ## Another Example Of Continue Statement
 b = 0
